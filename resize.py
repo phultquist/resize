7497b785effc8cccc05b30accb5e04615532305e
phultquist <phultquist@imsa.edu>
2020-08-25 19:02:38 -0500

    new format

--- resize.py
+++ resize.py
@@ -3,8 +3,18 @@ import numpy as np
 from io import BytesIO
 import requests
 
-# src = 'https://i.scdn.co/image/ab67616d00004851988ede5e1276e758b5f9e577'
+
+#ydb
+src = 'https://i.scdn.co/image/ab67616d00004851988ede5e1276e758b5f9e577'
+
+#graduation
 src = 'resize/test.png'
+
+#xxx album
+src = 'https://i.scdn.co/image/ab67616d00004851806c160566580d6335d1f16c'
+
+#mbdtf
+src = 'https://i.scdn.co/image/ab67616d00004851d9194aa18fa4c9362b47464f'
 content = src
 if src.startswith('http'):
     imgresp = requests.get(src)
@@ -15,44 +25,68 @@ pix = np.array(img)
 width, height = img.size
 stepsize = int(width / 16)
 
+
 def generalize(pixels, avg):
-    totals = [0,0,0,0]
-    if avg:    
+    totals = [0, 0, 0, 0]
+    if avg:
         for j in range(len(pixels)):
             pixel = pixels[j]
-            totals[0] += pixel[0] #r
-            totals[1] += pixel[1] #g
-            totals[2] += pixel[2] #b
+            try:
+                totals[0] += pixel[0]  # r
+                totals[1] += pixel[1]  # g
+                totals[2] += pixel[2]  # b
+            except:
+                totals[0] += pixel  # r
+                totals[1] += pixel  # g
+                totals[2] += pixel  # b
             # totals[3] += pixel[3] #alpha
-        
+
         for k in range(len(totals)):
             totals[k] = totals[k] / len(pixels)
         totals[3] = 255
-    else: 
+    else:
         reds = []
         greens = []
         blues = []
         for j in range(len(pixels)):
             pixel = pixels[j]
-            reds.append(pixel[0])
-            greens.append(pixel[1])
-            blues.append(pixel[2])
-        
+            try:
+                reds.append(pixel[0])
+                greens.append(pixel[1])
+                blues.append(pixel[2])
+            except:
+                reds.append(pixel)
+                greens.append(pixel)
+                blues.append(pixel)
+
         red = np.median(np.array(reds))
         green = np.median(np.array(greens))
         blue = np.median(np.array(blues))
 
         totals = [red, green, blue, 255]
-    
+
     for k in range(len(totals)):
-            if (totals[k] > 255):
-                totals[k] = 255
-            totals[k] = np.uint8(totals[k])
+        if (totals[k] > 255):
+            totals[k] = 255
+        totals[k] = np.uint8(contrast(totals[k]))
     return totals
+
+def contrast(o):
+    m = 255
+    g = 1.1
+    
+    if o == 0 or o == 255 or o == m/2:
+        return o
+    g2 = 1/g
+    if o < m/2:
+        return -1 * ((m/2) * ((2*(-1*o+m/2)/m) ** g2) - m/2)
+    elif o > m/2:
+        return ((m/2) * ((2*(o-m/2)/m) ** g2) + m/2)
+
 
 x = 0
 resized = []
-while x < height: 
+while x < height:
     y = 0
     newrow = []
     while y < width:
@@ -63,7 +97,7 @@ while x < height:
         for a in range(4):
             for b in range(4):
                 toGeneralize.append(pix[x+a][y+b])
-        
+
         genavg = generalize(toGeneralize, True)
         genmed = generalize(toGeneralize, False)
         gend = []
@@ -74,7 +108,7 @@ while x < height:
         print(genmed)
         print(gend)
         print('  ')
-        newrow.append(genmed)
+        newrow.append(genavg)
         y += stepsize
 
     # print('x', x, '   y', y, '   len', newrow)
@@ -87,3 +121,5 @@ im.show()
 print(len(pix))
 print(len(pix[0]))
 print(len(pix[0][0]))
+
+
